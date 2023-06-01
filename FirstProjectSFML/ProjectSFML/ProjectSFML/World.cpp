e9d8be31a57941993afa3799b98be2d5e1f93cb7
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-06-01 15:13:23 +0200

    basic view and introduction to the tilemap

--- FirstProjectSFML/ProjectSFML/ProjectSFML/World.cpp
+++ FirstProjectSFML/ProjectSFML/ProjectSFML/World.cpp
@@ -9,6 +9,7 @@ using namespace std;
 AssetsManager assetsManager;
 
 string Tab[17][30];
+sf::RectangleShape tileMap[17][30];
 
 void World::TextureInitialization()
 {
@@ -42,6 +43,17 @@ void World::TileMapInitialization()
         }
     }
 
+    for (int i = 0; i < 17; i++)
+    {
+        for (int j = 0; j < 30; j++)
+        {
+            const sf::Texture texcure= assetsManager.GetTextureAsset(Tab[i][j]).TextureSFML;
+            int* texctureW = *texcure;
+            tileMap[i][j].setTexture(texcure);
+            tileMap[i][j].setPosition(i * 16.f, j * 16.f);
+        }
+    }
+
     file.close();
 }
 
@@ -51,6 +63,11 @@ void World::LoadWorld()
     World::TileMapInitialization();
 
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
+
+    sf::View mainView;
+    mainView.setSize(1920, 1080); 
+    mainView.setCenter(window.getSize().x / 2.f, window.getSize().y / 2.f);
+
     sf::Sprite firstSprite;
     int x = 0, y = 0;
 
@@ -64,10 +81,25 @@ void World::LoadWorld()
         }
 
         window.clear();
-        firstSprite.setTexture(assetsManager.GetTextureAsset(Tab[1][0])->TextureSFML);
-        firstSprite.setPosition(x, y);
-        window.draw(firstSprite);
-        int i = 0;
+        window.setView(mainView);
+
+        //Render game elements
+
+        //firstSprite.setTexture(assetsManager.GetTextureAsset(Tab[1][0])->TextureSFML);
+        //firstSprite.setPosition(x, y);
+        //window.draw(firstSprite);
+
+        for (int i = 0; i < 17; i++)
+        {
+            for (int j = 0; j < 30; j++)
+            {
+                window.draw(tileMap[i][j]);
+            }
+        }
+
+        window.setView(window.getDefaultView());
+
+        //Draw UI 
 
         window.display();
     }
