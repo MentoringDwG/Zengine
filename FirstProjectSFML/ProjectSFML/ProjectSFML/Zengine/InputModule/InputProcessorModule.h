0158b1d745d568b825bd5ae7223782592781a93a
Przemysław Machniewski <mach3p@hotmail.com>
2023-06-02 22:27:22 +0200

    Addiing missing InputProcessorModule.h

---
+++ FirstProjectSFML/ProjectSFML/ProjectSFML/Zengine/InputModule/InputProcessorModule.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../../IEngineModule.h"
+#include <vector>
+#include <SFML/Window/Event.hpp>
+#include "InputHandler.h"
+
+class InputProcessorModule : IEngineModule
+{
+public:
+	virtual void Initialize() override;
+	virtual void UnInitialize() override;
+
+	void ProcessInput(sf::Event& event);
+
+	void RegisterInputHandler(InputHandler* InInputHandler);
+	std::vector<InputHandler*> InputHandlers;
+};
