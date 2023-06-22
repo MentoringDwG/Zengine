297a52dfe65577f57f27839d1fb8c5d4abc9fa4c
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-06-22 18:28:43 +0200

    Adding id to InputHandler

--- ProjectSFML/Zengine/InputModule/InputHandler.h
+++ ProjectSFML/Zengine/InputModule/InputHandler.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <SFML/Window/Event.hpp>
+#include <iostream>
+
+using namespace std;
 
 class InputHandler
 {
 public:
+	int ID;
 	virtual bool ProcessInput(sf::Event& event) = 0;
 	virtual bool CanConsumeInput() = 0;
 };
