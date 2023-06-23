c7b6b74bcffb2d5a6959733749e1bbb7bcf025e3
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-06-23 15:06:53 +0200

    Correction of InputHandler class according to comments

--- ProjectSFML/Zengine/InputModule/InputHandler.h
+++ ProjectSFML/Zengine/InputModule/InputHandler.h
@@ -1,13 +1,12 @@
 #pragma once
 #include <SFML/Window/Event.hpp>
-#include <iostream>
-
-using namespace std;
 
 class InputHandler
 {
 public:
-	int ID;
 	virtual bool ProcessInput(sf::Event& event) = 0;
 	virtual bool CanConsumeInput() = 0;
+
+private:
+	int ID;
 };
