244b6a84a082e648df87d457d3e491a7cc3a5fe2
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-09-30 10:29:42 +0200

    new levels from tiled

---
+++ Mario/Tiles/Level1/Level1_3/spriteSheetLevel1_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="spriteSheetLevel1_3" tilewidth="16" tileheight="16" tilecount="50" columns="10">
+ <image source="spriteSheetLevel1_3.png" width="170" height="80"/>
+</tileset>
